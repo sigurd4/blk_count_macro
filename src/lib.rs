ce1debb3dd8eab32e99f4fa2db315ba99ac15b99
Sigurd <sigurd-s@online.no>
2023-03-15 22:38:34 +0100

    Upload

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+/// Counts number of comma-separated elements
+/// 
+/// Elements may be any token
+/// 
+/// ```rust
+/// const COUNT: usize = count!(0, f32, const, something);
+/// const COUNT_NONE: usize = count!();
+/// 
+/// assert_eq!(COUNT, 4);
+/// assert_eq!(COUNT_NONE, 0);
+/// ```
+#[macro_export]
+macro_rules! count {
+    () => (0usize);
+    ( $x:tt $(,$xs:tt)* ) => (1usize + count!($($xs),*));
+}
